86dcf7cabc92965d7c2b692072583bbd2fee1d3f
AustineIgunza <Austine.igunza@strathmore.edu>
2025-06-20 12:04:00 +0300

    bubble and selection sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,10 +1,8 @@
 def bubble_sort(unsorted_list):
     n = len(unsorted_list)
     for i in range(n):
-        # Last i elements are already sorted
         for j in range(0, n - i - 1):
             if unsorted_list[j] > unsorted_list[j + 1]:
-                # Swap if the element found is greater
                 unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
     return unsorted_list
 
